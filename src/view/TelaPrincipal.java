6e42c413e77e40641fb79665ec21785e68794344
Enzo S. Magalhães <es.magalhaes24@gmail.com>
2025-06-07 23:39:47 -0300

    Adicionada a Tela de Criação de Cursos

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -39,11 +39,10 @@ public class TelaPrincipal extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton btnDisciplinas = new JButton("Disciplinas");
 		btnDisciplinas.setForeground(Color.WHITE);
 		btnDisciplinas.setBackground(new Color(26, 95, 180));
@@ -51,15 +50,23 @@ public class TelaPrincipal extends JFrame {
 		btnDisciplinas.setFont(new Font("Dialog", Font.BOLD, 14));
 		btnDisciplinas.setBounds(290, 108, 125, 25);
 		contentPane.add(btnDisciplinas);
-		
+
 		JButton btnCursos = new JButton("Cursos");
+		btnCursos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TelaCursos telaCursos = new TelaCursos();
+				telaCursos.setVisible(true);
+				dispose();
+			}
+		});
+
 		btnCursos.setForeground(Color.WHITE);
 		btnCursos.setBackground(new Color(26, 95, 180));
 		btnCursos.setToolTipText("Acessar Cursos");
 		btnCursos.setFont(new Font("Dialog", Font.BOLD, 14));
 		btnCursos.setBounds(153, 108, 125, 25);
 		contentPane.add(btnCursos);
-		
+
 		JButton btnProfessores = new JButton("Professores");
 		btnProfessores.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -74,7 +81,7 @@ public class TelaPrincipal extends JFrame {
 		btnProfessores.setFont(new Font("Dialog", Font.BOLD, 14));
 		btnProfessores.setBounds(16, 108, 125, 25);
 		contentPane.add(btnProfessores);
-		
+
 		JButton btnProcessos = new JButton("Processos");
 		btnProcessos.setForeground(Color.WHITE);
 		btnProcessos.setBackground(new Color(26, 95, 180));
@@ -82,12 +89,12 @@ public class TelaPrincipal extends JFrame {
 		btnProcessos.setFont(new Font("Dialog", Font.BOLD, 14));
 		btnProcessos.setBounds(427, 108, 125, 25);
 		contentPane.add(btnProcessos);
-		
+
 		JLabel lblNewLabel = new JLabel("Sistema de Gerenciamento De Contratações");
 		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 20));
 		lblNewLabel.setBounds(80, 12, 455, 36);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblSejaBemVindo = new JLabel("Bem Vindo!");
 		lblSejaBemVindo.setFont(new Font("Dialog", Font.BOLD, 13));
 		lblSejaBemVindo.setBounds(255, 60, 85, 17);
