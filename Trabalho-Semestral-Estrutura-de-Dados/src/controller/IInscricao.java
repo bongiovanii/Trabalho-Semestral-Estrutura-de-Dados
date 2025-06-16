3a39367b2065636769753c4b4157881b960f1ee9
Gustavo Bongiovani <132799669+bongiovanii@users.noreply.github.com>
2025-06-15 21:03:54 -0300

    Delete Trabalho-Semestral-Estrutura-de-Dados directory

--- Trabalho-Semestral-Estrutura-de-Dados/src/controller/IInscricao.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-public interface IInscricao {
-	public void novaInscricao(long cpfProfessor, int codigoDisciplina, int codigoProcesso);
-	public void salvarInscricao(long cpfProfessor, int codigoDisciplina, int codigoProcesso);
-	
-}
