aab886455f69e57c4994a80fd0045f9cee445114
Gustavo Bongiovani <guhhfive@outlook.com>
2025-06-16 14:46:29 -0300

    Revert "Merge branch 'mudanças(Enzo)'"
    This reverts commit db21b6ce93165955808251f12e7066e23ba18f03, reversing
    changes made to 9fcfd3a324b735403aa9b891c6c2f30315f723dd.

--- src/controller/IInscricao.java
+++ src/controller/IInscricao.java
@@ -2,11 +2,7 @@ package controller;
 
 public interface IInscricao {
 	public void novaInscricao(long cpfProfessor, int codigoDisciplina, int codigoProcesso);
-<<<<<<< Updated upstream
-	public void salvarInscricao(long cpfProfessor, int codigoDisciplina, int codigoProcesso);
-=======
 	public void removeInscricao(int codigoProcesso);
 	
->>>>>>> Stashed changes
 	
 }
